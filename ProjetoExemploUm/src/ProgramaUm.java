6f5cc2a6ed790905ac60f56f9e8c72b3a18e23fb
gbarbosa04 <gabriel.barb.dos.santos@gmail.com>
2026-03-05 21:41:36 -0300

    ProgramaUm.java

--- ProjetoExemploUm/src/ProgramaUm.java
+++ ProjetoExemploUm/src/ProgramaUm.java
@@ -4,10 +4,13 @@ public class ProgramaUm {
 	public static void main(String[] args) {
 	System.out.print("First!");
 	
-	Veiculo veiculo = new Veiculo();
+	Veiculo v1 = new Veiculo();
+	Veiculo v2 = new Veiculo();
+	Veiculo v3 = new Veiculo();
 	
-	veiculo.id=87;
-
+	Moto m1 = new Moto();
+	m1.id=76;
+	m1.guidao="pilota-muito";
 	}
 
 }
